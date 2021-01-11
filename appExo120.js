c207b18fba01e85fb2165c13704dbdcd008e0f8d
Sylvie-B <unsolo@hotmail.fr>
2021-01-12 00:57:38 +0100

    switch losange

--- appExo120.js
+++ appExo120.js
@@ -1,17 +1,26 @@
+// fill all frames
 $('#fill').click(function (){
     $('.empty').addClass('full').removeClass('empty');
 });
 
+// empty all frames
 $('#clear').click(function (){
     $('.full').addClass('empty').removeClass('full');
 });
 
+// switch fill/empty
 $('.round').click(function (event){
     $(this).toggleClass('full empty');
 });
 
+// switch square
+$('.square').click(function (event){
+    $(this).parent().children().toggleClass('full empty');
+});
 
-// $('#fill').click(function (){
-//     $('.line > div').toggleClass('full empty');
-// });
-//
+// switch lozenge
+$('#lozenge').click(function (){
+    $('.loz').toggleClass('full empty');
+    $('.line3 > div').toggleClass('full empty');
+});
+
